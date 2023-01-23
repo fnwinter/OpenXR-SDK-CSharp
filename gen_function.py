3129ac1bbe3a69000139a17f6cde4e8169b1338a
JJL <fnwinter@gmail.com>
2023-01-23 23:11:42 +0900

    function

--- gen_function.py
+++ gen_function.py
@@ -14,6 +14,22 @@ def get_enum_key_value(e):
     _r = re.compile("(XR.*) = (.*[^,])[,\n]", re.MULTILINE)
     return _r.findall(e)
 
+
+def get_function_name(f):
+    _r = re.compile("(\w+(\s)?)\(", re.MULTILINE)
+    m = _r.findall(f)
+    return m[0][0].strip()
+
+def get_params(f):
+    _r = re.compile("\(((.|\n)*?)\)", re.MULTILINE)
+    m = _r.findall(f)
+    print(m[0][0])
+    print("-----------")
+    pass
+
+def get_return_type(f):
+    pass
+
 def gen_function(functions):
     with XrTemplate("function_template.txt") as xr_function:
         function_file_path = os.path.join(SCRIPT_PATH, "csharp", "openxr_function.cs")
@@ -21,7 +37,7 @@ def gen_function(functions):
         render_data = []
 
         for f in functions:
-            #print(f)
+            print(get_function_name(f))
             pass
        
         function_text = xr_function.render(render_data = render_data)
